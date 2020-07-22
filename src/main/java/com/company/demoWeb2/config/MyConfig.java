f5cd7595921ea065348483eeb00b746e91997659
xujia <jxu17@best-inc.com>
2020-07-22 17:55:16 +0800

    'xiugai'

--- src/main/java/com/company/demoWeb2/config/MyConfig.java
+++ src/main/java/com/company/demoWeb2/config/MyConfig.java
@@ -1,8 +1,11 @@
 package com.company.demoWeb2.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-//@ComponentScan(basePackages = {"com.company.demoWeb"})
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.company.demoWeb2"})
 public class MyConfig {
 }
---
+++ src/main/java/com/company/demoWeb2/config/MyWebInit.java
@@ -0,0 +1,24 @@
+package com.company.demoWeb2.config;
+
+import com.company.demoWeb2.config.MyConfig;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebInit implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        AnnotationConfigWebApplicationContext dispatcherContext =
+                new AnnotationConfigWebApplicationContext();
+        dispatcherContext.register(MyConfig.class);
+
+        DispatcherServlet dispatcher = new DispatcherServlet(dispatcherContext);
+        ServletRegistration.Dynamic sr = servletContext.addServlet("dispatcher", dispatcher);
+        sr.setLoadOnStartup(1);
+        sr.addMapping("/");
+    }
+}
