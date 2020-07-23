c66c229c7a4bae42bafa450a8e333e8dedd7fabc
xujia <jxu17@best-inc.com>
2020-07-23 21:16:03 +0800

    'xiugai'

---
+++ src/main/java/com/company/demoWeb2/aspect/MyProxyService2.java
@@ -0,0 +1,29 @@
+package com.company.demoWeb2.aspect;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class MyProxyService2 {
+
+    @Pointcut("execution(* *..*.PersonService.*(..))")
+    private void MyPt() {}
+
+    @Around("MyPt()")
+    public Object aroundProxyMethod(ProceedingJoinPoint pjp) {
+        Object re = null;
+        try {
+            System.out.println("前置通知");
+            re = pjp.proceed();//业务逻辑
+            System.out.println("后置通知");
+        } catch (Throwable throwable) {
+            System.out.println("异常通知");
+        } finally {
+            System.out.println("最终通知");
+        }
+        return re;
+    }
+}
--- src/main/java/com/company/demoWeb2/config/MyConfig.java
+++ src/main/java/com/company/demoWeb2/config/MyConfig.java
@@ -2,10 +2,12 @@ package com.company.demoWeb2.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.company.demoWeb2.controller"})
+@ComponentScan(basePackages = {"com.company.demoWeb2"})
+@EnableAspectJAutoProxy
 public class MyConfig {
 }
