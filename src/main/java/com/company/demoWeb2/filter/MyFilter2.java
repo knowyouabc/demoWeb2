b88048f9799e765e43d81aa8c426b9fb7153af13
xujia <jxu17@best-inc.com>
2020-07-23 18:05:21 +0800

    'xiugai'

---
+++ src/main/java/com/company/demoWeb2/filter/MyFilter2.java
@@ -0,0 +1,30 @@
+package com.company.demoWeb2.filter;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+public class MyFilter2 implements Filter {
+
+        @Override
+        public void init(FilterConfig fc) {
+            //一般用于加载资源
+            String a = "aa";
+        }
+
+        @Override
+        public void doFilter(ServletRequest srq, ServletResponse srp, FilterChain fc) throws IOException, ServletException {
+            //执行内容1，主要是统一操作request
+            String a = "aa";
+            fc.doFilter(srq, srp);//表示过滤操作完之后放行
+            //放行的页面执行完成后，继续执行内容2，主要是统一操作response
+        }
+
+        @Override
+        public void destroy() {
+            //一般用于释放资源
+            String a = "aa";
+        }
+
+}
