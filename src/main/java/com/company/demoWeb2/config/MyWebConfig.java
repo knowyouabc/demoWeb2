49e5d1fb9c08f2d63df1105d8f53e9d62f006e57
knowyouabc <6337606@qq.com>
2020-07-28 21:29:39 +0800

    'xiugai'

--- src/main/java/com/company/demoWeb2/config/MyWebConfig.java
+++ src/main/java/com/company/demoWeb2/config/MyWebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.company.demoWeb2"},
-        excludeFilters={@ComponentScan.Filter(type=FilterType.ANNOTATION,value={Controller.class})})
+@ComponentScan(basePackages = {"com.company.demoWeb2.controller"})
 @ImportResource(locations = {"classpath:app-config.xml"})
+//@ComponentScan(basePackages = {"com.company.demoWeb2"},
+//        excludeFilters={@ComponentScan.Filter(type=FilterType.ANNOTATION,value={Controller.class})})
 //@EnableAspectJAutoProxy
 //@PropertySource({"classpath:mysql/mysql.properties", "classpath:redis/redis.properties"})
-public class MyConfig {
+public class MyWebConfig {
 }
--- src/test/java/PersonServiceTest.java
+++ src/test/java/PersonServiceTest.java
@@ -1,9 +1,6 @@
-import com.company.demoWeb2.config.MyConfig;
 import com.company.demoWeb2.service.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
